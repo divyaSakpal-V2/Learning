6ca8efd1209da2a2beeb63c10ac32eae3bdac9c2
divyaSakpal-V2 <divya.sakpal@v2solutions.com>
2025-07-25 15:26:55 +0530

    first commit without bin

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Get Key Vault URL from configuration
+var keyVaultUrl = builder.Configuration["AzureKeyVault:VaultUri"];
+// Add Azure Key Vault to the ConfigurationBuilder
+
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+    // Retrieve and load secrets into IConfiguration
+    var secrets = client.GetPropertiesOfSecrets();
+    foreach (var secret in secrets)
+    {
+        var secretValue = client.GetSecret(secret.Name);
+        builder.Configuration[secret.Name] = secretValue.Value.Value;
+    }
+}
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
