7075ac0f72e8e7bfa2976bce83e2ca7bd7b36069
divyaSakpal-V2 <divya.sakpal@v2solutions.com>
2025-07-25 17:06:31 +0530

    Program file changes

--- Program.cs
+++ Program.cs
@@ -11,14 +11,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Get Key Vault URL from configuration
-
+var keyVaultUrl = builder.Configuration["AzureKeyVault:VaultUri"];
 // Add Azure Key Vault to the ConfigurationBuilder
 
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
 
+    // Retrieve and load secrets into IConfiguration
+    var secrets = client.GetPropertiesOfSecrets();
+    foreach (var secret in secrets)
+    {
+        var secretValue = client.GetSecret(secret.Name);
+        builder.Configuration[secret.Name] = secretValue.Value.Value;
+    }
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
