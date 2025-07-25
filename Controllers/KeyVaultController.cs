2c21ad15165bc2936ca21713d40c518e1ae43f37
divyaSakpal-V2 <divya.sakpal@v2solutions.com>
2025-07-25 16:52:38 +0530

    adding controllers

---
+++ Controllers/KeyVaultController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace LearningProject1.Controllers
+{
+
+    [Route("api/")]
+    [ApiController]
+    public class KeyVaultController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public KeyVaultController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("secrets/{secretName}")]
+        public string Get(string secretName)
+        {
+            var secretValue = _configuration[secretName];
+            return secretValue;
+        }
+    }
+}
---
+++ Controllers/TopicsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningProject1.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicsController : ControllerBase
+    {
+        List<string> Topics;
+        public TopicsController()
+        {
+            Topics = new List<string>() { "Azure", "Dot net" };
+        }
+
+        // GET: api/<TopicsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Topics;
+        }
+
+        // GET api/<TopicsController>/5
+        [HttpGet("{name}")]
+        public IEnumerable<string> Get(string name)
+        {
+            return Topics.FindAll(x => x.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
